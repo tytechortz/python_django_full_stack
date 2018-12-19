2649098ca09cfbe5875b784a913a161b8e7c107c
James <tytechortz@gmail.com>
2018-12-19 11:49:22 -0700

    for and while loops

--- JS_level_one/myscript.js
+++ JS_level_one/myscript.js
@@ -1,10 +1,28 @@
-var x = 0;
 
-while ( x < 11) {
+// for(i = 0; i < 5; i++) {
+//     console.log('hello')
+// }
 
-    if (x % 2 === 0) {
-        console.log(x)
-    }
-    x = x + 1;
+// var x = 0;
+// while(x<5) {
+//     console.log('hello');
+//     x = x+1;
+// }
+
+
+
+
+var x = 1;
+
+while(x < 26) {
+    console.log(x);
+    x = x+2;
 }
+
+
+
+// for(i = 1; i < 27; i=i+2) {
+//     console.log(i)
+// }
+
  
