be48b98389fb7a641516f7df262936aa53bc6b43
James <tytechortz@gmail.com>
2018-12-18 17:42:22 -0700

    prit even numbers

--- JS_level_one/myscript.js
+++ JS_level_one/myscript.js
@@ -1,4 +1,10 @@
-alert("Welcome to your bank");
-var deposit = prompt("How much would you like to deposit?");
-alert ("You deposited: "+deposit)
-console.log("You are cool");
+var x = 0;
+
+while ( x < 11) {
+
+    if (x % 2 === 0) {
+        console.log(x)
+    }
+    x = x + 1;
+}
+ 
