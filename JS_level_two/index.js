4c8314bce6f56137b707459d7d803f244bd363a5
James <tytechortz@gmail.com>
2018-12-20 10:21:57 -0700

    problem 3

--- JS_level_two/index.js
+++ JS_level_two/index.js
@@ -9,14 +9,24 @@
 
 // employee.nameLength()
 
+// var employee = {
+//   name: "John Smith",
+//   job: "Programmer",
+//   age: 31,
+//   employeeAlert: function(){
+//     alert('Name is: '+this.name+ ', job is: '+this.job+ ', age is: '+this.age)
+//   }
+// };
+
+// employee.employeeAlert()
+
 var employee = {
   name: "John Smith",
   job: "Programmer",
   age: 31,
-  employeeAlert: function(){
-    alert('Name is: '+this.name+ ', job is: '+this.job+ ', age is: '+this.age)
+  lastName: function(){
+    console.log(this.name.split(" ")[1])
   }
 };
 
-employee.employeeAlert()
-
+employee.lastName()
