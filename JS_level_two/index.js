0ab458944ac8d4d05929dc8ea8f46f8876a893a5
James <tytechortz@gmail.com>
2018-12-19 19:13:04 -0700

    finish roster problem

--- JS_level_two/index.js
+++ JS_level_two/index.js
@@ -1,20 +1,40 @@
 var roster = [];
 
 const addNew = () => {
-  name = prompt('Add name:')
+  var name = prompt('Add name:')
   roster.push(name)
 }
 
-addNew();
-console.log(roster)
+
 
 const remove = () => {
-  removeName = prompt('Name to remove:');
+  var removeName = prompt('Name to remove:');
   var index = roster.indexOf(removeName);
-  if (index > -1) {
     roster.splice(index, 1);
-  }
 }
 
-remove();
-console.log(roster)
+
+
+const display = () => {
+  console.log(roster);
+}
+
+
+
+
+let use = prompt('Would you like to use the roster app? y/n')
+let request = 'empty';
+
+if (use === 'y') {
+  while ( request !== "quit") {
+    request = prompt("Please select an action: add, remove, display, or quit.")
+    if (request === 'add') {
+      addNew();
+    } else if (request === 'display') {
+      display();
+    } else if (request === 'remove') {
+      remove();
+    }
+  }
+}
+alert("Thanks for using this web app");
