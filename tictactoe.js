5c078fbfee0dbe1b667b2d23d3f239385e8342b8
James <tytechortz@gmail.com>
2018-12-20 14:30:11 -0700

    finishes game

--- tictactoe.js
+++ tictactoe.js
@@ -9,3 +9,19 @@ function clearBoard() {
 }
 
 restart.addEventListener('click',clearBoard);
+
+function changeMarker(){
+    if(this.textContent === ''){
+        this.textContent = 'X';
+    } else if (this.textContent = 'X') {
+        this.textContent = 'O';
+    } else {
+        this.textContent = '';
+    }
+};
+
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker);
+};
+
+console.log('connected')
