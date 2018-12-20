8ae64daa208cd91a3870ef2e3cc6b018e6bd62dc
James <tytechortz@gmail.com>
2018-12-19 19:00:09 -0700

    remove name from roster

--- JS_level_two/index.js
+++ JS_level_two/index.js
@@ -7,3 +7,14 @@ const addNew = () => {
 
 addNew();
 console.log(roster)
+
+const remove = () => {
+  removeName = prompt('Name to remove:');
+  var index = roster.indexOf(removeName);
+  if (index > -1) {
+    roster.splice(index, 1);
+  }
+}
+
+remove();
+console.log(roster)
