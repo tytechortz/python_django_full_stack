be072dcc25de75e58b312eca22d897d5f61c4bd1
James <tytechortz@gmail.com>
2018-12-20 09:59:28 -0700

    part 6 exercises prob 1

--- JS_level_two/index.js
+++ JS_level_two/index.js
@@ -1,40 +1,10 @@
-var roster = [];
-
-const addNew = () => {
-  var name = prompt('Add name:')
-  roster.push(name)
-}
-
-
-
-const remove = () => {
-  var removeName = prompt('Name to remove:');
-  var index = roster.indexOf(removeName);
-    roster.splice(index, 1);
-}
-
-
-
-const display = () => {
-  console.log(roster);
-}
-
-
-
-
-let use = prompt('Would you like to use the roster app? y/n')
-let request = 'empty';
-
-if (use === 'y') {
-  while ( request !== "quit") {
-    request = prompt("Please select an action: add, remove, display, or quit.")
-    if (request === 'add') {
-      addNew();
-    } else if (request === 'display') {
-      display();
-    } else if (request === 'remove') {
-      remove();
-    }
+var employee = {
+  name: "John Smith",
+  job: "Programmer",
+  age: 31,
+  nameLength: function(){
+    console.log(this.name.length)
   }
 }
-alert("Thanks for using this web app");
+
+employee.nameLength()
