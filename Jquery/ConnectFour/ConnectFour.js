c597f6201263331375eee0bcfe1f0f1da9cf41e8
James <tytechortz@gmail.com>
2018-12-26 15:08:04 -0700

    starts js file, player names

---
+++ Jquery/ConnectFour/ConnectFour.js
@@ -0,0 +1,8 @@
+var player1 = prompt("Player One: Enter Your Name , you will be Blue");
+var player1Color = 'rgb(86, 151, 255)';
+
+var player2 = prompt("Player Two: Enter Your Name, you will be Red");
+var player2Color = 'rgb(237, 45, 73)';
+
+var game_on = true;
+var table = $('table tr');
