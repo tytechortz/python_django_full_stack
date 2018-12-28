0fa30ae563b89fdd885b0994648de00df71ae407
James <tytechortz@gmail.com>
2018-12-28 14:27:59 -0700

    problem 5

--- Python/Part6_Exercise_Review.py
+++ Python/Part6_Exercise_Review.py
@@ -62,8 +62,10 @@ print(set(mylist))
 ###############
 
 # You are given two variables:
-# age = 4
-# name = "Sammy"
+age = 4
+name = "Sammy"
+
 
 # Use print formatting to print the following string:
 # "Hello my dog's name is Sammy and he is 4 years old"
+print("Hello my dog's name is " + name + " and he is "+ str(age) +" years old")
