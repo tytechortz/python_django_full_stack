d5a6c087d725add8e36a04fd422e6b2cfecb2003
James <tytechortz@gmail.com>
2018-12-28 14:28:29 -0700

    problem 5

--- Python/Part6_Exercise_Review.py
+++ Python/Part6_Exercise_Review.py
@@ -68,4 +68,4 @@ name = "Sammy"
 
 # Use print formatting to print the following string:
 # "Hello my dog's name is Sammy and he is 4 years old"
-print("Hello my dog's name is " + name + " and he is "+ str(age) +" years old")
+print("Hello my dog's name is {a} and he is {b} years old.".format(a=age,b=name))
