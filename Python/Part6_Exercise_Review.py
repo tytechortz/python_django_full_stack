b01eaee3d3deedf2d19ae11033e7533fa3125ac1
James <tytechortz@gmail.com>
2018-12-28 14:15:00 -0700

    problem 3

--- Python/Part6_Exercise_Review.py
+++ Python/Part6_Exercise_Review.py
@@ -24,7 +24,7 @@ print(s[1:4])
 # 'go'
 print(s[4:])
 # Bonus: Use indexing to reverse the string
-
+print(s[::-1])
 
 ###############
 ## Problem 2 ##
@@ -43,18 +43,18 @@ print(l)
 # Using keys and indexing, grab the 'hello' from the following dictionaries:
 
 d1 = {'simple_key':'hello'}
-
+print(d1['simple_key'])
 d2 = {'k1':{'k2':'hello'}}
-
+print(d2['k1']['k2'])
 d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
-
+print(d3['k1'][0]['nest_key'][1][0])
 
 ###############
 ## Problem 4 ##
 ###############
 
 # Use a set to find the unique values of the list below:
-mylist = [1,1,1,1,1,2,2,2,2,3,3,3,3]
+# mylist = [1,1,1,1,1,2,2,2,2,3,3,3,3]
 
 
 ###############
@@ -62,8 +62,8 @@ mylist = [1,1,1,1,1,2,2,2,2,3,3,3,3]
 ###############
 
 # You are given two variables:
-age = 4
-name = "Sammy"
+# age = 4
+# name = "Sammy"
 
 # Use print formatting to print the following string:
-"Hello my dog's name is Sammy and he is 4 years old"
+# "Hello my dog's name is Sammy and he is 4 years old"
