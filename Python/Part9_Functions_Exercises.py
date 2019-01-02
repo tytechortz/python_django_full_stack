1a0e009da6472aae4a667a27e94df674c7bc7437
James <tytechortz@gmail.com>
2019-01-02 12:11:32 -0700

    problem 6

--- Python/Part9_Functions_Exercises.py
+++ Python/Part9_Functions_Exercises.py
@@ -142,5 +142,12 @@ def fix_teen(n):
 # count_evens([2, 2, 0]) → 3
 # count_evens([1, 3, 5]) → 0
 
-# def count_evens(nums):
-  # CODE GOES HERE
+def count_evens(nums):
+  count = 0
+
+  for i in nums:
+    if i%2 == 0:
+      count += 1
+  print(count)
+
+count_evens([2,1,2,3,4])
