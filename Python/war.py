86fe9738740e9ca734ec757a1cd4629b164bdfcf
James <tytechortz@gmail.com>
2019-01-07 10:07:11 -0700

    creates deck

--- Python/war.py
+++ Python/war.py
@@ -4,8 +4,26 @@ from random import shuffle
 SUITE = 'H D S C'.split()
 RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
 
-class Deck:
-    pass
+mycards = [(s,r) for s in SUITE for r in RANKS]
+print(mycards)
+print(len(mycards))
 
-x = Deck()
-print(type(x))
+class Deck:
+    def __init__(self):
+        print("Creating Deck")
+        self.allcards = [(s,r) for s in SUITE for r in RANKS ]
+
+    def shuffle(self):
+        print("Shuffling Deck")
+        shuffle(self.allcards)
+
+    def split_in_half(self):
+        print("Splitting Deck")
+        return (self.allcards[:26], self.allcards[26:])
+
+class Hand:
+    def __init__(self,cards):
+        self.cards = cards
+
+    def __str__(self):
+        return "Contains {} cards".format(len(self.cards))
