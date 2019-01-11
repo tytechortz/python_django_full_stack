4e471ba31eb28e7b04d3e1059875f8233b3dec4d
James <tytechortz@gmail.com>
2019-01-10 21:03:02 -0700

    templates challenge

--- Django_Stuff/ProTwo/AppTwo/views.py
+++ Django_Stuff/ProTwo/AppTwo/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<em>My Second App</em>')
+    help = {'insert_me': "I am from index.html"}
+    return render(request, 'ProTwo/index.html',context=help)
