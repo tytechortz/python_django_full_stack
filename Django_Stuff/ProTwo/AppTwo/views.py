95983f6dd0894f485aec806fc6dd01e7373f520a
James <tytechortz@gmail.com>
2019-01-12 11:45:29 -0700

    finishes level 2

--- Django_Stuff/ProTwo/AppTwo/views.py
+++ Django_Stuff/ProTwo/AppTwo/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from appTwo.models import User
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<em>My Second Project</em>")
+    return render(request,'appTwo/index/html')
 
-def help(request):
-    helpdict = {'help_insert': "HELP PAGE"}
-    return render(request, 'apptwo/help.html',context=helpdict)
+def users(request):
+    user_list = User.objects.order_by('first_name')
+    user_dict = {'users':user_list}
+    return render(request, 'appTwo/users.html',context=user_dict)
--- Django_Stuff/ProTwo/ProTwo/settings.py
+++ Django_Stuff/ProTwo/ProTwo/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ProTwo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
