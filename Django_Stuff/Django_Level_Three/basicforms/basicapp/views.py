e496abf087fe733b3d5252018e967b6a2f2b1b5e
James <tytechortz@gmail.com>
2019-01-12 12:48:59 -0700

    finishes basic form

--- Django_Stuff/Django_Level_Three/basicforms/basicapp/views.py
+++ Django_Stuff/Django_Level_Three/basicforms/basicapp/views.py
@@ -7,5 +7,16 @@ def index(request):
 
 def form_name_view(request):
     form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            # DO SOMETHING CODE
+            print("Validation success!")
+            print("NAME: "+form.cleaned_data['name'])
+            print("EMAIL: "+form.cleaned_data['email'])
+            print("TEXT: "+form.cleaned_data['text'])
+
     return render(request, 'basicapp/form_page.html',{'form':form})
 
