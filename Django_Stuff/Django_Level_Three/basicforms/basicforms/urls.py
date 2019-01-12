f7efee1b025316853a4b89b1d0605cbb8a46701f
James <tytechortz@gmail.com>
2019-01-12 12:25:08 -0700

    starts app

--- Django_Stuff/Django_Level_Three/basicforms/basicforms/urls.py
+++ Django_Stuff/Django_Level_Three/basicforms/basicforms/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from basicapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$',views.index,name='index'),
+    url('admin/', admin.site.urls),
+    url(r'^formpage/',views.form_name_view,name='form_name')
 ]
