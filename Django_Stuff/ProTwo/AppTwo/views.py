e281eccd713114755615abe09c34e579a216ca31
James <tytechortz@gmail.com>
2019-01-13 10:11:40 -0700

    part 3 challenge

--- Django_Stuff/ProTwo/AppTwo/views.py
+++ Django_Stuff/ProTwo/AppTwo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from appTwo.models import User
+#from appTwo.models import User
+from appTwo.forms import NewUserForm
 
 # Create your views here.
 
@@ -7,6 +8,17 @@ def index(request):
     return render(request,'appTwo/index/html')
 
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users':user_list}
-    return render(request, 'appTwo/users.html',context=user_dict)
+    
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('Error-invalid id')
+
+    return render(request,'appTwo/users.html',{'form':form})
+        
